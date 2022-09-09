d1d358c6354b94add86feabccd8c41d9703a9d0d
Gareth Tribello <gareth.tribello@gmail.com>
2022-09-09 14:59:58 +0100

    Fixes to testing framework

--- testing/test_main.py
+++ testing/test_main.py
@@ -1,17 +1,17 @@
 try:
-    import AutoFeedback.varchecks as vc
-except: 
+    from AutoFeedback.funcchecks import check_func
+except:
     import subprocess
     import sys
 
     subprocess.check_call([sys.executable, "-m", "pip", "install", "AutoFeedback"])
-    import AutoFeedback.varchecks as vc
+    from AutoFeedback.funcchecks import check_func
 
+from AutoFeedback.randomclass import randomvar
 import unittest
 from main import *
 
 class UnitTests( unittest.TestCase ): 
    def test_service_func(self):
-      st_func = qn.edge2queue[0].service_f
-      myvar = randomvar( 2, variance=4, nsamples=100 )
-      assert( check_func( st_func, [(,)], [myvar] ) )
+      myvar = randomvar( 2, variance=4, nsamples=10 )
+      assert( check_func( "st_func", [(0,)], [myvar] ) )
